00fa76cb7fda3408c25765580814386e19cf4aa2
bobasyu <bobasyu@163.com>
2021-04-14 10:16:37 +0800

    摄像头操作

--- TCPLinkClient.py
+++ TCPLinkClient.py
@@ -2,6 +2,9 @@ import socket
 import logging
 import threading
 
+import motion
+from InstructionEnum import InstructionEnum
+
 
 class TCPLinkClient(threading.Thread):
     """
@@ -11,7 +14,8 @@ class TCPLinkClient(threading.Thread):
     def __init__(self, port: int = 8888):
         threading.Thread.__init__(self)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = '192.168.199.195'  # socket.gethostname()
+        self.host = '81.68.230.143'  # socket.gethostname()
+        # self.host = '127.0.0.1'  # socket.gethostname()
         self.port = port
         try:
             self.socket.connect((self.host, self.port))
@@ -25,6 +29,12 @@ class TCPLinkClient(threading.Thread):
             if data:
                 msg = bytes.decode(data, encoding='utf-8')
                 print(msg)
+                if msg == str(InstructionEnum.OPEN_MOTION.value):
+                    logging.info('打开摄像头')
+                    motion.open_motion()
+                elif msg == str(InstructionEnum.CLOSE_MOTION):
+                    logging.info('关闭摄像头')
+                    motion.stop_motion()
 
     def send(self, data: str):
         self.socket.send(data.encode(encoding='utf-8'))
